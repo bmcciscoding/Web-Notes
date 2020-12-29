50af2800636c2b09a141e3e954e7a6593471fe30
QP <bmccisccoding@gmail.com>
2020-12-29 16:07:00 +0800

    use prettier

--- React/react-demo/src/Compoents/Clock.js
+++ React/react-demo/src/Compoents/Clock.js
@@ -1,47 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Clock extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     console.log(props);
 
     // props.newDate = new Date() // 编译器会报错：TypeError: Cannot add property newDate, object is not extensible
 
     this.state = {
-       date: props.date
+      date: props.date,
     };
+  }
 
-  };
-
-    componentDidMount() {
-      setTimeout(() => {
-              this.setState({
-      didMount: true
-    })
-      }, 2000);
-
-  };
-  
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        didMount: true,
+      });
+    }, 2000);
+  }
 
   render() {
     return (
       <div>
         <p>
-          <div>
-            const string: This is a clock
-          </div>
-          <div>
-            DidMount {this.state.didMount ? 'yes' : 'no'}
-          </div>
-          <div>
-            prop: {this.props.date.toLocaleTimeString()}
-          </div>
-          <div>
-            state: {this.state.date.toLocaleTimeString()}
-          </div>
+          <div>const string: This is a clock</div>
+          <div>DidMount {this.state.didMount ? "yes" : "no"}</div>
+          <div>prop: {this.props.date.toLocaleTimeString()}</div>
+          <div>state: {this.state.date.toLocaleTimeString()}</div>
         </p>
       </div>
-    )
+    );
   }
 }
