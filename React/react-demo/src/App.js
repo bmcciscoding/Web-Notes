fdb2a56edbb1a63bc577f44fb171322714416b68
QP <bmccisccoding@gmail.com>
2020-12-29 16:04:35 +0800

    add react state & props

--- React/react-demo/src/App.js
+++ React/react-demo/src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Clock from './Compoents/Clock'
+
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <myheader/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -18,6 +26,7 @@ function App() {
           Learn React
         </a>
       </header>
+        <Clock date={new Date()}/>
     </div>
   );
 }
