47d82ddb9232604c6359eafcaf1fcc54026c2555
QP <bmccisccoding@gmail.com>
2021-01-05 18:20:13 +0800

    add web notes

--- React/react-demo/src/Compoents/Clock.js
+++ React/react-demo/src/Compoents/Clock.js
@@ -36,6 +36,7 @@ export default class Clock extends Component {
     // props.newDate = new Date() // 编译器会报错：TypeError: Cannot add property newDate, object is not extensible
 
     this.state = {
+      hide: true,
       date: props.date,
       text: "I am a react demo",
       useTop: false,
@@ -72,41 +73,54 @@ export default class Clock extends Component {
     console.log("tap sub\n", p1, e);
   }
 
+  showContent() {
+    const hide = this.state.hide;
+    this.setState({
+      hide: !hide,
+    });
+  }
+
   render() {
+    const hide = this.state.hide;
     return (
       <div>
-        <p>
-          <div>const string: This is a clock</div>
-          <div>DidMount {this.state.didMount ? "yes" : "no"}</div>
-          <div>prop: {this.props.date.toLocaleTimeString()}</div>
-          <div>state: {this.state.date.toLocaleTimeString()}</div>
-          <button onClick={this.changeTop.bind(this, "p1")}>
-            {this.state.useTop
-              ? this.state.text.toLocaleUpperCase()
-              : this.state.text.toLocaleLowerCase()}
-          </button>
+        <button onClick={this.showContent.bind(this)}>
+          {hide ? "show" : "hide"}
+        </button>
+        {hide ? null : (
+          <p>
+            <div>const string: This is a clock</div>
+            <div>DidMount {this.state.didMount ? "yes" : "no"}</div>
+            <div>prop: {this.props.date.toLocaleTimeString()}</div>
+            <div>state: {this.state.date.toLocaleTimeString()}</div>
+            <button onClick={this.changeTop.bind(this, "p1")}>
+              {this.state.useTop
+                ? this.state.text.toLocaleUpperCase()
+                : this.state.text.toLocaleLowerCase()}
+            </button>
 
-          <h2>
-            事件绑定
-            <button onClick={this.changeTop}>this.changeTop</button>
-            <button onClick={this.changeTop.bind(this)}>
-              this.changeTop.bind(this
-            </button>
-            <button onClick={this.changeTop.bind(this, 5)}>
-              this.changeTop.bind(this, 5)
-            </button>
-            <button onClick={(e) => this.changeTop(5, e)}>
-              (e) => this.changeTop(5, e)
-            </button>
-            <SubClock onTap={this.handleSubComEevent} />
-          </h2>
+            <h2>
+              事件绑定
+              <button onClick={this.changeTop}>this.changeTop</button>
+              <button onClick={this.changeTop.bind(this)}>
+                this.changeTop.bind(this
+              </button>
+              <button onClick={this.changeTop.bind(this, 5)}>
+                this.changeTop.bind(this, 5)
+              </button>
+              <button onClick={(e) => this.changeTop(5, e)}>
+                (e) => this.changeTop(5, e)
+              </button>
+              <SubClock onTap={this.handleSubComEevent} />
+            </h2>
 
-          <h2>
-            渲染
-            <IfConditionDiv isLog={this.state.useTop}></IfConditionDiv>
-            <ListDiv lists={[1, 2, 3, 4, 5]} />
-          </h2>
-        </p>
+            <h2>
+              渲染
+              <IfConditionDiv isLog={this.state.useTop}></IfConditionDiv>
+              <ListDiv lists={[1, 2, 3, 4, 5]} />
+            </h2>
+          </p>
+        )}
       </div>
     );
   }
